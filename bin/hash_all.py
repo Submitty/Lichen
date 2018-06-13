ed024553ed45dc375a37e33de544f71ba74a78e9
Barb Cutler <bmcutler@users.noreply.github.com>
2018-06-12 23:15:41 -0400

    Rough draft of core plagiarism detection process (#5)
    * initial tokenize_all script
    
    * wip
    
    * first draft of hash comparison
    
    * wip
    
    * wip
    
    * fix nested directories in concatenate
    
    * improve comments for initial PR
    
    * tweak

---
+++ bin/hash_all.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+"""
+Walks the submission directory and creates a parallel directory of
+the tokenized files.
+
+"""
+
+import argparse
+import os
+import json
+import subprocess
+import sys
+import json
+import hashlib
+
+
+CONFIG_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'config')
+with open(os.path.join(CONFIG_PATH, 'submitty.json')) as open_file:
+    OPEN_JSON = json.load(open_file)
+SUBMITTY_DATA_DIR = OPEN_JSON['submitty_data_dir']
+SUBMITTY_INSTALL_DIR = OPEN_JSON['submitty_install_dir']
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("semester")
+    parser.add_argument("course")
+    parser.add_argument("gradeable")
+    parser.add_argument("--window",type=int,default=10)
+    parser.add_argument("--hash_size",type=int,default=100000)
+    language = parser.add_mutually_exclusive_group(required=True)
+    language.add_argument ("--plaintext", action='store_true')
+    language.add_argument ("--python", action='store_true')
+    language.add_argument ("--cpp", action='store_true')
+
+    args = parser.parse_args()
+
+    if (args.window < 1):
+        print ("ERROR! window must be >= 1")
+        exit(1)
+    
+    return args
+
+
+def hasher(args,my_tokenized_file,my_hashes_file):
+    with open(my_tokenized_file,'r') as my_tf:
+        with open(my_hashes_file,'w') as my_hf:
+            tokens = json.load(my_tf)
+            num = len(tokens)
+            for i in range(0,num-args.window):
+                foo=""
+                if args.plaintext:
+                    for j in range(0,args.window):
+                        foo+=str(tokens[i+j].get("value"))
+                elif args.python:
+                    print("NEED A PYTHON HASHER")
+                elif args.cpp:
+                    print("NEED A C++ HASHER")
+                else:
+                    print("UNKNOWN HASHER")
+                hash_object = hashlib.md5(foo.encode())
+                hash_object_string=hash_object.hexdigest()
+                #FIXME: this truncation should be adjusted after more full-scale testing
+                hash_object_string_truncated=hash_object_string[0:4]
+                #my_hf.write(hash_object_string+"\n")
+                my_hf.write(hash_object_string_truncated+"\n")
+
+
+def main():
+    args = parse_args()
+
+    sys.stdout.write("HASH ALL...")
+    sys.stdout.flush()
+    
+    # ===========================================================================
+    # error checking
+    course_dir=os.path.join(SUBMITTY_DATA_DIR,"courses",args.semester,args.course)
+    if not os.path.isdir(course_dir):
+        print("ERROR! ",course_dir," is not a valid course directory")
+        exit(1)
+    tokenized_dir=os.path.join(course_dir,"lichen","tokenized",args.gradeable) 
+    if not os.path.isdir(tokenized_dir):
+        print("ERROR! ",tokenized_dir," is not a valid gradeable tokenized directory")
+        exit(1)
+
+    hashes_dir=os.path.join(course_dir,"lichen","hashes",args.gradeable)
+
+    # ===========================================================================
+    # walk the subdirectories
+    for user in os.listdir(tokenized_dir):
+        for version in os.listdir(os.path.join(tokenized_dir,user)):
+            my_tokenized_file=os.path.join(tokenized_dir,user,version,"tokens.json")
+
+            # ===========================================================================
+            # create the directory
+            my_hashes_dir=os.path.join(hashes_dir,user,version)
+            if not os.path.isdir(my_hashes_dir):
+                os.makedirs(my_hashes_dir)
+
+            my_hashes_file=os.path.join(my_hashes_dir,"hashes.txt")
+            hasher(args,my_tokenized_file,my_hashes_file)
+
+
+    print("done")
+            
+if __name__ == "__main__":
+    main()
---
+++ bin/tokenize_all.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+"""
+Tokenizes the concatenated files.
+"""
+
+import argparse
+import os
+import json
+import subprocess
+import sys
+
+
+CONFIG_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'config')
+with open(os.path.join(CONFIG_PATH, 'submitty.json')) as open_file:
+    OPEN_JSON = json.load(open_file)
+SUBMITTY_DATA_DIR = OPEN_JSON['submitty_data_dir']
+SUBMITTY_INSTALL_DIR = OPEN_JSON['submitty_install_dir']
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("semester")
+    parser.add_argument("course")
+    parser.add_argument("gradeable")
+    language = parser.add_mutually_exclusive_group(required=True)
+    language.add_argument ("--plaintext", action='store_true')
+    language.add_argument ("--python", action='store_true')
+    language.add_argument ("--cpp", action='store_true')
+    return parser.parse_args()
+
+
+def tokenize(args,my_concatenated_file,my_tokenized_file):
+
+    if args.plaintext:
+        tokenizer = os.path.join(SUBMITTY_INSTALL_DIR,"Lichen","bin","plaintext_tokenizer.out")
+        with open(my_concatenated_file,'r') as infile:
+            with open (my_tokenized_file,'w')as outfile:
+                subprocess.call([tokenizer,"--ignore_newlines"],stdin=infile,stdout=outfile)
+    elif args.python:
+        print("NEED A PYTHON TOKENIZER")
+    elif args.cpp:
+        print("NEED A C++ TOKENIZER")
+    else:
+        print("UNKNOWN TOKENIZER")
+
+def main():
+    args = parse_args()
+
+    sys.stdout.write("TOKENIZE ALL...")
+    sys.stdout.flush()
+    
+    # ===========================================================================
+    # error checking
+    course_dir=os.path.join(SUBMITTY_DATA_DIR,"courses",args.semester,args.course)
+    if not os.path.isdir(course_dir):
+        print("ERROR! ",course_dir," is not a valid course directory")
+        exit(1)
+    concatenated_dir=os.path.join(course_dir,"lichen","concatenated",args.gradeable)
+    if not os.path.isdir(concatenated_dir):
+        print("ERROR! ",concatenated_dir," is not a valid gradeable concatenated directory")
+        exit(1)
+
+    tokenized_dir=os.path.join(course_dir,"lichen","tokenized",args.gradeable)
+
+    # ===========================================================================
+    # walk the subdirectories
+    for user in os.listdir(concatenated_dir):
+        for version in os.listdir(os.path.join(concatenated_dir,user)):
+            my_concatenated_file=os.path.join(concatenated_dir,user,version,"submission.concatenated")
+
+            # ===========================================================================
+            # create the directory
+            my_tokenized_dir=os.path.join(tokenized_dir,user,version)
+            if not os.path.isdir(my_tokenized_dir):
+                os.makedirs(my_tokenized_dir)
+
+            my_tokenized_file=os.path.join(my_tokenized_dir,"tokens.json")
+            tokenize(args,my_concatenated_file,my_tokenized_file)
+
+    print ("done")
+
+    
+if __name__ == "__main__":
+    main()

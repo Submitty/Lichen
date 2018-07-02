95b52ffbab3093d3323195eed42508f1b321b88d
tushargr <tushar.gurjar1999@gmail.com>
2018-07-03 05:15:14 +0530

    remove empty value tokens

--- tokenizer/python/python_tokenizer.py
+++ tokenizer/python/python_tokenizer.py
@@ -1,7 +1,7 @@
 import parso
-from parso.python.tokenize import tokenize 
+from parso.python.tokenize import tokenize
 import json
-import sys 
+import sys
 
 file = open(sys.argv[1],'r')
 file_content = file.read()
@@ -9,12 +9,12 @@ file_content = file.read()
 tokens = []
 
 for token in tokenize(file_content, version_info=(3,6)):
-	tmp = dict()
-	tmp["line"]=(token.start_pos)[0];
-	tmp["char"]=((token.start_pos)[1])+1;
-	tmp["type"]=((str(token.type))[10:]).strip(")")
-	tmp["value"]=str(token.string)
-	tokens.append(tmp)
+        if (str(token.string) != ""):
+                tmp = dict()
+                tmp["line"]=(token.start_pos)[0];
+                tmp["char"]=((token.start_pos)[1])+1;
+                tmp["type"]=((str(token.type))[10:]).strip(")")
+                tmp["value"]=str(token.string)
+                tokens.append(tmp)
 
 print ( json.dumps(tokens, indent=4, sort_keys=True) )
-

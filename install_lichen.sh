7d7a25abf9f72077448c4ebec37d5d9533dcf442
Andrew Aikens <andrewaikens87@gmail.com>
2020-03-28 11:27:07 -0400

    [Tools] Adds assignment placer to place files in pre-existing submissions  (#20)
    * Tools
    
    * fixed
    
    * assign Placer tweak
    
    * fix install.sh
    
    * update assignment_placer
    
    * tweaks
    
    * small twekas
    
    * fix hardcode semester
    
    * Update assignment_placer.py
    
    Co-authored-by: Matthew Peveler <matt.peveler@gmail.com>

--- install_lichen.sh
+++ install_lichen.sh
@@ -30,7 +30,7 @@ fi
 # compile & install the tools
 
 mkdir -p ${lichen_installation_dir}/bin
-
+mkdir -p ${lichen_installation_dir}/tools/assignments
 
 #--------------------
 # plaintext tool
@@ -64,6 +64,7 @@ cp ${lichen_repository_dir}/tokenizer/java/java_tokenizer.py ${lichen_installati
 cp ${lichen_repository_dir}/tokenizer/mips/mips_tokenizer.py ${lichen_installation_dir}/bin/mips_tokenizer.py
 cp ${lichen_repository_dir}/tokenizer/data.json ${lichen_installation_dir}/bin/data.json
 
+cp -rf ${lichen_repository_dir}/tools/* ${lichen_installation_dir}/tools
 
 ########################################################################################################################
 # fix permissions

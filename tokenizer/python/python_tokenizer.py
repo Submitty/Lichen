eced30e308521ded220c38591474653b88b7b999
williamjallen <dalearn@icloud.com>
2021-06-01 08:42:18 -0400

    More fixes

--- tokenizer/python/python_tokenizer.py
+++ tokenizer/python/python_tokenizer.py
@@ -1,20 +1,19 @@
-import parso
 from parso.python.tokenize import tokenize
 import json
 import sys
 
-file = open(sys.argv[1],'r')
+file = open(sys.argv[1], 'r')
 file_content = file.read()
 
 tokens = []
 
-for token in tokenize(file_content, version_info=(3,6)):
+for token in tokenize(file_content, version_info=(3, 6)):
         if (str(token.string) != ""):
                 tmp = dict()
-                tmp["line"]=(token.start_pos)[0];
-                tmp["char"]=((token.start_pos)[1])+1;
-                tmp["type"]=((str(token.type))[10:]).strip(")")
-                tmp["value"]=str(token.string)
+                tmp["line"] = (token.start_pos)[0]
+                tmp["char"] = ((token.start_pos)[1]) + 1
+                tmp["type"] = ((str(token.type))[10:]).strip(")")
+                tmp["value"] = str(token.string)
                 tokens.append(tmp)
 
-print ( json.dumps(tokens, indent=4, sort_keys=True) )
+print(json.dumps(tokens, indent=4, sort_keys=True))

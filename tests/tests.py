c50e01711723c0a2dd12c9c3198fd3f6c8515dcc
williamjallen <dalearn@icloud.com>
2021-07-03 11:43:45 -0400

    fix path

--- tests/tests.py
+++ tests/tests.py
@@ -13,7 +13,7 @@ class TestPlaintextTokenizer(unittest.TestCase):
         output_file = f"{lichen_data_dir}/f21/test_tokenizers/lichen/plaintext_tokenizer_tests/output.json"
         # expected_output_file = f"{lichen_repository_dir}/tests/data/tokenizer/plaintext/output.json"
 
-        command = f"{lichen_installation_dir}/plaintext_tokenizer.out {input_file} > {output_file}"
+        command = f"{lichen_installation_dir}plaintext_tokenizer.out {input_file} > {output_file}"
         os.system(command)
 
         with open(output_file) as file:

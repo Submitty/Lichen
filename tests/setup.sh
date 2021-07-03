0595eb396d3dd415227abd9f2ff0063069aaba93
williamjallen <dalearn@icloud.com>
2021-07-03 11:31:07 -0400

    Adjust file structure, add setup script

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
-mkdir -p /usr/local/submitty/GIT_CHECKOUT/Lichen/
-cp -r * /usr/local/submitty/GIT_CHECKOUT/Lichen/
+lichen_repository_dir=/usr/local/submitty/GIT_CHECKOUT/Lichen/
+lichen_installation_dir=/usr/local/submitty/Lichen/
+lichen_data_dir=/var/local/submitty/courses/
 
-mkdir -p /usr/local/submitty/Lichen/
+# make a simulated GIT_CHECKOUT directory
+mkdir -p $lichen_repository_dir
+cp -r * $lichen_repository_dir
+cd $lichen_repository_dir
 
-bash /usr/local/submitty/GIT_CHECKOUT/Lichen/install_lichen.sh
+# install Lichen
+mkdir -p $lichen_installation_dir
+bash $lichen_repository_dir/install_lichen.sh
+
+# SETUP TOKENIZER TESTS ########################################################
+tokenizer_tests_course=$lichen_data_dir/f21/test_tokenizers/lichen/
+# make a simulated lichen path for the test_tokenizers course
+mkdir -p $tokenizer_tests_course
+
+# set up file structure for plaintext tokenizer tests
+# (doesn't need a full file structure, just a place to put files)
+mkdir -p $tokenizer_tests_course/plaintext_tokenizer_tests/
--- tests/tests.py
+++ tests/tests.py
@@ -1,10 +1,24 @@
 import unittest
+import os
+
+lichen_repository_dir = "/usr/local/submitty/GIT_CHECKOUT/Lichen/"
+lichen_installation_dir = "/usr/local/submitty/Lichen/"
+lichen_data_dir = "/var/local/submitty/courses/"
 
 
-class TestTokenizers(unittest.TestCase):
-
+class TestPlaintextTokenizer(unittest.TestCase):
     def testPlaintextTokenizer(self):
-        print('test!')
+        input_file = f"{lichen_repository_dir}/tests/data/tokenizer/plaintext/input.txt"
+        output_file = f"{lichen_data_dir}/f21/test_tokenizers/lichen/plaintext_tokenizer_tests/output.json"
+        # expected_output_file = f"{lichen_repository_dir}/tests/data/tokenizer/plaintext/output.json"
+
+        command = f"{lichen_installation_dir}/plaintext_tokenizer.out {input_file} > {output_file}"
+        os.system(command)
+
+        with open(output_file) as file:
+            print(file.read())
+
+        os.remove(output_file)
 
 
 if __name__ == '__main__':

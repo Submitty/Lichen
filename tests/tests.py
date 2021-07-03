5956ec4fedec06c7517f547e3d420fae43103669
williamjallen <dalearn@icloud.com>
2021-07-03 11:57:04 -0400

    fix more paths

--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,9 @@
 import unittest
 import os
 
-lichen_repository_dir = "/usr/local/submitty/GIT_CHECKOUT/Lichen/"
-lichen_installation_dir = "/usr/local/submitty/Lichen/"
-lichen_data_dir = "/var/local/submitty/courses/"
+lichen_repository_dir = "/usr/local/submitty/GIT_CHECKOUT/Lichen"
+lichen_installation_dir = "/usr/local/submitty/Lichen"
+lichen_data_dir = "/var/local/submitty/courses"
 
 
 class TestPlaintextTokenizer(unittest.TestCase):
@@ -12,7 +12,7 @@ class TestPlaintextTokenizer(unittest.TestCase):
         output_file = f"{lichen_data_dir}/f21/test_tokenizers/lichen/plaintext_tokenizer_tests/output.json"
         expected_output_file = f"{lichen_repository_dir}/tests/data/tokenizer/plaintext/output.json"
 
-        command = f"{lichen_installation_dir}bin/plaintext_tokenizer.out < {input_file} > {output_file}"
+        command = f"{lichen_installation_dir}/bin/plaintext_tokenizer.out < {input_file} > {output_file}"
         os.system(command)
 
         with open(output_file) as file:

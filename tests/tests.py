39f786233d2504a6f47cf7a51959ae754ee03de2
Shelly Belsky <71195502+sbelsk@users.noreply.github.com>
2021-07-22 10:18:16 -0400

    [Testing:Plagiarism] Add test for hash_all.py (#41)
    * Initial rewrite of process_all.sh
    
    * Update process_all.sh
    
    * Make modifications to file paths and add timers
    
    * Overhaul concatenate_all.py
    
    * Fix python errors
    
    * Progress: everything through tokenization finished
    
    * Everything works
    
    * Add timers
    
    * remove unnecessary code
    
    * little python changes
    
    * William made an oopsie (forgot to deal with provided code)
    
    * Fix minor bugs
    
    Fix process_all.sh script plus fix spelling issue and prevent hash_all.py from breaking when empty tokenized files are written
    
    * Fix permissions issue with provided code editing
    
    * Add initial script
    
    * Update lichen_run.yml
    
    * Update lichen_run.yml
    
    * Update lichen_run.yml
    
    * Update lichen_run.yml
    
    * Update lichen_run.yml
    
    * add boost
    
    * add testing file
    
    * forgot that paths are important
    
    * Make separate setup.sh script
    
    * Update lichen_run.yml
    
    * Adjust file structure, add setup script
    
    * need sudo for test
    
    * Update tests.py
    
    * fix path
    
    * fix path
    
    * Update tests.py
    
    * add assertion to implement test
    
    * fix more paths
    
    * fix another path issue
    
    * Add second test
    
    * Update tests.py
    
    * it's important to run the right command to get the right results...
    
    * Add third test
    
    * Add remaining plaintext tokenizer tests
    
    * Add C tokenizer tests
    
    * Update lichen_run.yml
    
    * Update lichen_run.yml
    
    * Update lichen_run.yml
    
    * Add MIPS tokenizer
    
    * Update tests.py
    
    * Update tests.py
    
    * Fix paths in tests.py such that it can be run in vagrant
    
    * Fix github actions
    
    * Add hash all test
    
    * change paths
    
    * Get rid of unwanted stdout
    
    * Remove old code
    
    * comment with missing letter was bugging me
    
    Co-authored-by: williamjallen <dalearn@icloud.com>
    Co-authored-by: Barb Cutler <bmcutler@users.noreply.github.com>

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 import shutil
+import json
 
 lichen_installation_dir = "/usr/local/submitty/Lichen"
 lichen_test_playground = "/usr/local/submitty/Lichen/test_output"
@@ -130,6 +131,63 @@ class TestMIPSTokenizer(unittest.TestCase):
             expected_output = file.read()
 
         self.assertEqual(actual_output, expected_output)
+
+
+class TestHashAll(unittest.TestCase):
+    def setUp(self):
+        if not os.path.isdir(lichen_test_playground):
+            os.makedirs(lichen_test_playground)
+
+    def tearDown(self):
+        shutil.rmtree(lichen_test_playground)
+
+    def testHashAll(self):
+        # make the fake directory structure hash_all.py expects
+        os.makedirs(f"{lichen_test_playground}/test_hash_all/provided_code")
+        os.makedirs(f"{lichen_test_playground}/test_hash_all/other_gradeables")
+        os.makedirs(f"{lichen_test_playground}/test_hash_all/users/student/1")
+        open(f"{lichen_test_playground}/test_hash_all/config.json", 'a').close()
+        open(f"{lichen_test_playground}/test_hash_all/users/student/1/tokens.json", 'a').close()
+        with open(f"{lichen_test_playground}/test_hash_all/provided_code/tokens.json", 'w') as file:
+            file.write("null")
+
+        # copy the input files from /data to the the new path
+        shutil.copyfile("data/hash_all/config.json", f"{lichen_test_playground}/test_hash_all/config.json")
+        shutil.copyfile("data/hash_all/tokens.json", f"{lichen_test_playground}/test_hash_all/users/student/1/tokens.json")
+
+        # save current working directory
+        cwd = os.getcwd()
+
+        # run hash_all
+        os.chdir(f"{lichen_installation_dir}/bin")
+        os.system(f"python3 {lichen_installation_dir}/bin/hash_all.py {lichen_test_playground}/test_hash_all > /dev/null")
+        os.chdir(cwd)
+
+        # test output
+        hashes_file = f"{lichen_test_playground}/test_hash_all/users/student/1/hashes.txt"
+        with open(hashes_file, 'r') as file:
+            lines = file.readlines()
+        lines = [x.strip() for x in lines]
+        tokens_file = f"{lichen_test_playground}/test_hash_all/users/student/1/tokens.json"
+        with open(tokens_file, 'r') as file:
+            tokens = json.load(file)
+
+        # make sure the number of sequences and the number of hashes are the same
+        self.assertEqual(len(lines), len(tokens) - 2 + 1)
+
+        # make sure the same sequences hash to the same string, and
+        # that different sequences hash to different strings
+        for i in range(0, len(lines)):
+            for j in range(i + 1, len(lines)):
+                if i == 4 and j == 9\
+                 or i == 4 and j == 16\
+                 or i == 9 and j == 16\
+                 or i == 13 and j == 22\
+                 or i == 14 and j == 23\
+                 or i == 15 and j == 24:
+                    self.assertEqual(lines[i], lines[j])
+                else:
+                    self.assertNotEqual(lines[i], lines[j])
 
 
 if __name__ == '__main__':

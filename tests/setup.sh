947dffa6ce414690feb9a18f556970df8458f363
William Allen <dalearn@icloud.com>
2021-07-22 10:15:53 -0400

    [Testing:Plagiarism] basic tokenizer tests (#40)
    * Initial rewrite of process_all.sh
    
    * Update process_all.sh
    
    * Make modifications to file paths and add timers
    
    * Overhaul concatenate_all.py
    
    * Fix python errors
    
    * Progress: everything through tokenization finished
    
    * Everything works
    
    * Add timers
    
    * remove unnecessary code
    
    * little python changes
    
    * William made an oopsie (forgot to deal with provided code)
    
    * Fix minor bugs
    
    Fix process_all.sh script plus fix spelling issue and prevent hash_all.py from breaking when empty tokenized files are written
    
    * Fix permissions issue with provided code editing
    
    * Add initial script
    
    * Update lichen_run.yml
    
    * Update lichen_run.yml
    
    * Update lichen_run.yml
    
    * Update lichen_run.yml
    
    * Update lichen_run.yml
    
    * add boost
    
    * add testing file
    
    * forgot that paths are important
    
    * Make separate setup.sh script
    
    * Update lichen_run.yml
    
    * Adjust file structure, add setup script
    
    * need sudo for test
    
    * Update tests.py
    
    * fix path
    
    * fix path
    
    * Update tests.py
    
    * add assertion to implement test
    
    * fix more paths
    
    * fix another path issue
    
    * Add second test
    
    * Update tests.py
    
    * it's important to run the right command to get the right results...
    
    * Add third test
    
    * Add remaining plaintext tokenizer tests
    
    * Add C tokenizer tests
    
    * Update lichen_run.yml
    
    * Update lichen_run.yml
    
    * Update lichen_run.yml
    
    * Add MIPS tokenizer
    
    * Update tests.py
    
    * Update tests.py
    
    * Fix paths in tests.py such that it can be run in vagrant
    
    * Fix github actions
    
    * Remove old code
    
    Co-authored-by: sbelsk <shellybelsky02@gmail.com>

---
+++ tests/setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+lichen_repository_dir=/usr/local/submitty/GIT_CHECKOUT/Lichen/
+lichen_installation_dir=/usr/local/submitty/Lichen/
+lichen_data_dir=/var/local/submitty/courses/
+
+# make a simulated GIT_CHECKOUT directory
+mkdir -p $lichen_repository_dir
+cp -r * $lichen_repository_dir
+cd $lichen_repository_dir
+
+# install Lichen
+mkdir -p $lichen_installation_dir
+bash $lichen_repository_dir/install_lichen.sh
+
+# SETUP TOKENIZER TESTS ########################################################
+tokenizer_tests_course=$lichen_data_dir/f21/test_tokenizers/lichen/
+# set up file structure for tokenizer tests
+# (doesn't need a full file structure, just a place to put files)
+mkdir -p $tokenizer_tests_course

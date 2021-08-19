2ca693bc2df226767c9b1a4ec2821e21ebcec0eb
William Allen <16820599+williamjallen@users.noreply.github.com>
2021-08-19 10:08:36 -0400

    [Bugfix:System] Fix permissions on pip packages (#60)

--- install_lichen.sh
+++ install_lichen.sh
@@ -27,6 +27,15 @@ apt-get install -y libboost-all-dev
 # python requirements
 pip install -r ${lichen_repository_dir}/requirements.txt
 
+# These permissions changes are copied from Submitty/.setup/INSTALL_SUBMITTY_HELPER.sh
+# Setting the permissions are necessary as pip uses the umask of the user/system, which
+# affects the other permissions (which ideally should be o+rx, but Submitty sets it to o-rwx).
+# This gets run here in case we make any python package changes.
+find /usr/local/lib/python*/dist-packages -type d -exec chmod 755 {} +
+find /usr/local/lib/python*/dist-packages -type f -exec chmod 755 {} +
+find /usr/local/lib/python*/dist-packages -type f -name '*.py*' -exec chmod 644 {} +
+find /usr/local/lib/python*/dist-packages -type f -name '*.pth' -exec chmod 644 {} +
+
 ########################################################################################################################
 # get tools/source code from other repositories
 

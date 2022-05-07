0b84d7964885e779a1f23db9b04d8d8a8a7ebe20
williamjallen <william.j.allen24@gmail.com>
2022-05-07 10:20:31 -0400

    Install Python dependencies for tokenizer tests

--- tests/setup.sh
+++ tests/setup.sh
@@ -2,28 +2,26 @@
 
 lichen_repository_dir=/usr/local/submitty/GIT_CHECKOUT/Lichen/
 lichen_installation_dir=/usr/local/submitty/Lichen/
-lichen_data_dir=/var/local/submitty/courses/
 
 # make a simulated GIT_CHECKOUT directory
-mkdir -p $lichen_repository_dir
-cp -r * $lichen_repository_dir
-cd $lichen_repository_dir
+mkdir -p "$lichen_repository_dir"
+cp -r * "$lichen_repository_dir"
+cd "$lichen_repository_dir"
 
-# Set up docker in advance
+# Install Python dependencies for unit tests
+pip install -r "$lichen_repository_dir"/requirements.txt
+
+# Setup docker
 sudo apt-get update
-apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+apt-get install ca-certificates curl gnupg lsb-release
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli
 
 # install Lichen
-mkdir -p $lichen_installation_dir
-bash $lichen_repository_dir/install_lichen.sh
+mkdir -p "$lichen_installation_dir"
+bash "$lichen_repository_dir"/install_lichen.sh

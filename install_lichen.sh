868e4dbf5d6fb7ca39d4ca2f6a3ba33b0f97c92d
williamjallen <william.j.allen24@gmail.com>
2022-05-09 10:54:41 -0400

    Install Python dependencies outside the Docker container as well

--- install_lichen.sh
+++ install_lichen.sh
@@ -13,6 +13,19 @@ lichen_installation_dir=/usr/local/submitty/Lichen
 
 cp -r "$lichen_repository_dir"/* "$lichen_installation_dir"
 
+# Install Python Dependencies locally (for concatenation)
+pip install -r /Lichen/requirements.txt
+
+# These permissions changes are copied from Submitty/.setup/INSTALL_SUBMITTY_HELPER.sh
+# Setting the permissions are necessary as pip uses the umask of the user/system, which
+# affects the other permissions (which ideally should be o+rx, but Submitty sets it to o-rwx).
+# This gets run here in case we make any python package changes.
+find /usr/local/lib/python*/dist-packages -type d -exec chmod 755 {} +
+find /usr/local/lib/python*/dist-packages -type f -exec chmod 755 {} +
+find /usr/local/lib/python*/dist-packages -type f -name '*.py*' -exec chmod 644 {} +
+find /usr/local/lib/python*/dist-packages -type f -name '*.pth' -exec chmod 644 {} +
+
+# Create the Docker container
 docker build -t lichen "$lichen_repository_dir"
 
 # fix permissions

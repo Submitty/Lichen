91bf97f5ecb71728d87b504d0ab99490940c7700
williamjallen <william.j.allen24@gmail.com>
2022-05-09 11:23:04 -0400

    attempt to get permissions working.....

--- tests/integration/setup.sh
+++ tests/integration/setup.sh
@@ -8,16 +8,16 @@ mkdir -p "$lichen_repository_dir"
 cp -r * "$lichen_repository_dir"
 
 # Setup docker
-sudo apt-get update
+apt-get update
 apt-get install ca-certificates curl gnupg lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli
+apt-get update
+apt-get install docker-ce docker-ce-cli
 
 # install Lichen
-sudo mkdir -p "$lichen_installation_dir"
+mkdir -p "$lichen_installation_dir"
 bash "$lichen_repository_dir"/install_lichen.sh
